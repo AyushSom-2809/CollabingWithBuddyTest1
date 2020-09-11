fcdbb0d0c2af9c887a440612d595b851ebc6f61f
Ayush <ayushsom10@gmail.com>
2020-09-11 14:58:57 +0530

    Will look forwad for my buddy to make a change in it

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollabWithBuddyTesting1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Testing with my buddy Aritro for collab and waiting for him to make a change !";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
